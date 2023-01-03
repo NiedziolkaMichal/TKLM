bcbe5a4f835863d4f06f23151d579af12140ffb6
Niedziolka Michal <niedziolek@hotmail.com>
2023-01-03 17:10:01 +0100

    Improving performance

--- src/components/util/floatingStars.tsx
+++ src/components/util/floatingStars.tsx
@@ -3,7 +3,6 @@ import styles from "../../../styles/Home.module.css";
 export function FloatingStars({}) {
   return (
     <div className={styles.stars}>
-      <div className={styles.small}></div>
       <div className={styles.medium}></div>
       <div className={styles.big}></div>
     </div>
