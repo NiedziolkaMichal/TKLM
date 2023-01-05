8d87f98656eed3e19c95c475f946ce0d2a5e01b3
Niedziolka Michal <niedziolek@hotmail.com>
2023-01-05 18:16:25 +0100

    Add Content-Security-Policy

--- next.config.js
+++ next.config.js
@@ -1,3 +1,49 @@
+const CSP_GENERAL = {
+  "default-src": ["'none'"],
+  "script-src": ["'self'"],
+  "style-src": ["'self'", "'unsafe-inline'"],
+  "font-src": ["'self'"],
+  "img-src": ["'self'", "data:"],
+  "media-src": ["'self'"],
+  "connect-src": ["'self'"],
+}
+const CSP_DEV = {
+  "script-src": ["'unsafe-eval'"]
+}
+
+const CSP_GOOGLE_MAPS = {
+  "script-src": ["https://*.googleapis.com"],
+  "style-src": ["https://fonts.googleapis.com"],
+  "font-src": ["https://fonts.gstatic.com"],
+  "img-src": ["https://*.googleapis.com", "https://*.gstatic.com", "*.google.com", "*.googleusercontent.com"],
+  "connect-src": ["https://*.googleapis.com", "*.google.com", "https://*.gstatic.com"],
+}
+
+function getCSPValue(pageHasGoogleMaps) {
+  const combinedDirectives = {};
+
+  function addDirectives(directives) {
+    for (const [directive, sources] of Object.entries(directives)) {
+      if (!combinedDirectives.hasOwnProperty(directive)) {
+        combinedDirectives[directive] = new Set(sources);
+      } else {
+        sources.forEach(s => combinedDirectives[directive].add(s))
+      }
+    }
+  }
+
+  addDirectives(CSP_GENERAL);
+  console.log('env', process.env.NODE_ENV)
+  if (process.env.NODE_ENV === 'development') {
+    addDirectives(CSP_DEV);
+  }
+  if (pageHasGoogleMaps) {
+    addDirectives(CSP_GOOGLE_MAPS)
+  }
+
+  return Object.entries(combinedDirectives).map(([k, v]) => k + ' ' + [...v].join(' ')).join(';');
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -39,8 +85,16 @@ const nextConfig = {
             key: "Strict-transport-security",
             value: "max-age=63072000; includeSubDomains" /* TODO Add preload */,
           },
-          /* TODO content-security-policy */
         ],
+      },
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: getCSPValue(true)
+          }
+        ]
       },
       {
         source: "/:path((?:img|font|favicon)/[\\s\\S]+|[^\/]+(?:\.svg|\.json|\.ico))",
